a3708956cce8d213af9de0b55a17d1dc08f55f98
FrLorenz <144800642+Xonatine@users.noreply.github.com>
2025-01-27 18:31:02 +0100

    Add files via upload

--- src/pipex.c
+++ src/pipex.c
@@ -12,6 +12,49 @@
 
 #include "../include/pipex.h"
 
+void check_envp(char **envp)
+{
+    int i;
+    int check;
+
+    i = -1;
+    check = 0;
+    while (envp[++i])
+        if (ft_strnstr(envp[i], "PATH=", 5) && envp[i][6])
+            check = 1;
+    if (!check)
+    {
+        custom_error("ERROR", "The enviroment PATH has no values.");
+        exit(1);
+    }
+}
+
+void sub_process(int *fd, char **argv, char **envp)
+{
+    int fd_in;
+
+    fd_in = open(argv[1], O_RDONLY, 0777);
+    if (fd_in == -1)
+        exit_error();
+    dup2(fd[1], STDOUT_FILENO);
+    dup2(fd_in, STDIN_FILENO);
+    close(fd[0]);
+    run_cmd(argv[2], envp);
+}
+
+void main_process(int *fd, char **argv, char **envp)
+{
+    int fd_out;
+
+    fd_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    if (fd_out == -1)
+        exit_error();
+    dup2(fd[0], STDIN_FILENO);
+    dup2(fd_out, STDOUT_FILENO);
+    close(fd[1]);
+    run_cmd(argv[3], envp);
+}
+
 int main(int argc, char **argv, char **envp)
 {
     int fd[2];
@@ -19,38 +62,20 @@ int main(int argc, char **argv, char **envp)
     
     if (argc == 5)
     {
-        pipe(fd);
-        pid = fork(); //!!!!!controlar el -1 por si falla!!!!!
-        if (pid == 0) //Codigo ejecutado por el HIJO 1
-        {
-            close(fd[1]); // cerrar el fd de lectura
-            printf("HIJO 1 => %i\n", pid);
-            exit(0); 
-        }
-        else //codigo ejecutado por el PADRE
-        {
-            close(fd[0]); //cerrar el fd de escritura
-            printf("PADRE => %i\n", pid);
-            pid = fork(); //!!!!!controlar el -1 por si falla!!!!!
-            if (pid == 0)
-            {
-                close(fd[1]);// cerrar el fd de lectura
-                printf("HIJO 2 => %i\n", pid);
-                exit(0); 
-            }
-            else // codigo ejecutado por el PADRE
-            {
-                close(fd[0]);//cerrar el fd de escritura
-                printf("PADRE => %i\n", pid);
-            }
-        }
+        if (pipe(fd) == 1)
+            exit_error();
+        pid = fork();
+        if (pid == -1)
+            exit_error();
+        if (pid == 0)
+            sub_process(fd, argv, envp);
+        waitpid(pid, NULL, 0);
+        main_process(fd, argv, envp);
     }
-    else //tienen que ser 5 argumentos ERROR !!CAMBIAR!!
+    else
     {
-        ft_printf("%s\n", argv[0]);
-        ft_printf("%s\n", envp[1]);
-        /*while(*envp)
-            ft_printf("%s\n", *envp++);*/
+        custom_error("Error", "Wrong number of arguments");
+        exit(1);
     }
     return (0);
 }
