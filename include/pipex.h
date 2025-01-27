ac0f8fbd4308a0cc9bcbb65deb3bc1970be208c0
FrLorenz <144800642+Xonatine@users.noreply.github.com>
2025-01-27 18:32:12 +0100

    Add files via upload

--- include/pipex.h
+++ include/pipex.h
@@ -10,12 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef  PIPPEX_H
+#ifndef  PIPEX_H
 # define PIPEX_H
 
 # include "../lib/libft/libft.h"
 # include <unistd.h>
 # include <stdio.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <signal.h>
 
+void run_cmd(char *cmd, char **envp);
+char *get_cmd_path(char *cmd, char **envp);
+void free_split(char **str);
+void exit_error(void);
+void custom_error(char *header, char *msg);
+void check_envp(char **envp);
+void sub_process(int *fd, char **argv, char **envp);
+void main_process(int *fd, char **argv, char **envp);
 
 #endif
